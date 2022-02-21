0ced523aab38c665b644629a6554c358148aeccf
Ryan <ryan.leichty@gmail.com>
2022-02-20 18:58:09 -0600

    Add constant for footer links

--- components/footer.js
+++ components/footer.js
@@ -1,32 +1,35 @@
-function FooterLink({ url, children }) {
-  return (
-    <div>
-      <a
-        href={url}
-        target='_blank'
-        rel='noreferrer'
-        className='inline-block py-2 sm:py-1 px-3 sm:p-2'
-      >
-        {children}
-      </a>
-    </div>
-  );
-}
+import { SOCIAL_PROFILES as socialProfiles } from '../constants/social';
 
 function Footer() {
+  const socialLinks = socialProfiles.map((profile) => {
+    return (
+      <div key={profile.name}>
+        <a
+          href={profile.slug}
+          target='_blank'
+          rel='noreferrer'
+          className='inline-block py-2 sm:py-1 px-3 sm:p-2'
+        >
+          {profile.name}
+        </a>
+      </div>
+    );
+  });
+
   return (
     <footer className='p-8 mt-auto grid grid-cols-2 items-center gap-2 text-xs sm:text-[10px] font-mono uppercase tracking-[2px] md:grid-cols-1'>
-      <FooterLink url='mailto:hello@ryanleichty.com'>
-        👋 hello@ryanleichty.com
-      </FooterLink>
+      <div>
+        <a
+          href='mailto:hello@ryanleichty.com'
+          target='_blank'
+          rel='noreferrer'
+          className='inline-block py-2 sm:py-1 px-3 sm:p-2'
+        >
+          👋 hello@ryanleichty.com
+        </a>
+      </div>
       <nav className='flex justify-end gap-4 md:flex-col md:justify-start  md:gap-2'>
-        <FooterLink url='https://www.instagram.com/ryanleichty/'>
-          Instagram
-        </FooterLink>
-        <FooterLink url='https://www.linkedin.com/in/ryanleichty/'>
-          LinkedIn
-        </FooterLink>
-        <FooterLink url='https://github.com/ryanleichty'>GitHub</FooterLink>
+        {socialLinks}
       </nav>
     </footer>
   );
---
+++ constants/social.js
@@ -0,0 +1,18 @@
+export const SOCIAL_PROFILES = [
+  {
+    name: 'Instagram',
+    slug: 'https://www.instagram.com/ryanleichty/',
+  },
+  {
+    name: 'Twitter',
+    slug: 'https://twitter.com/ryanleichty/',
+  },
+  {
+    name: 'LinkedIn',
+    slug: 'https://www.linkedin.com/in/ryanleichty/',
+  },
+  {
+    name: 'GitHub',
+    slug: 'https://github.com/ryanleichty/',
+  },
+];
