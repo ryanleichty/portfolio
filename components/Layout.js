d5c216eedc25aa3db7a1d83cf3f666fd3efe38b5
Ryan <ryan.leichty@gmail.com>
2022-02-22 12:21:47 -0600

    Remove top/bottom padding from main

--- components/Layout.js
+++ components/Layout.js
@@ -5,9 +5,7 @@ function Layout({ children }) {
   return (
     <div className='flex flex-col min-h-full'>
       <Header />
-      <main className='p-8 flex-1 flex flex-col md:min-h-[calc(100vh-96px)]'>
-        {children}
-      </main>
+      <main className='px-8 flex-1 flex flex-col'>{children}</main>
       <Footer />
     </div>
   );
