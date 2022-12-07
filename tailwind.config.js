5f7ecdfa00cfa8175090c54a7a1ceaf6a4fa2a13
Ryan Leichty <ryan.leichty@gmail.com>
2022-12-06 23:54:46 -0600

    Update config files and extend Tailwind theme
    - Added a custom CSS variable for the header height and the screen height minus the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,20 @@ const { colors } = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      minHeight: {
+        header: 'var(--header-height)',
+        screen: 'var(--screen-height, 100vh)',
+        'screen-no-header': 'calc(var(--screen-height, 100vh) - var(--header-height))',
+      },
+      height: {
+        header: 'var(--header-height)',
+        screen: 'var(--screen-height, 100vh)',
+        'screen-no-header': 'calc(var(--screen-height, 100vh) - var(--header-height))',
+      },
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
@@ -28,58 +40,87 @@ module.exports = {
       sans: ['var(--font-mabry)', ...fontFamily.sans],
     },
     fontSize: {
-      xs: '0.875rem',
-      sm: '1rem',
-      base: '1.25rem',
-      lg: '1.5rem',
-      xl: '2rem',
-      '2xl': '2.5rem',
-      '3xl': '3rem',
+      xs: [
+        '0.875rem',
+        {
+          lineHeight: '1.45',
+        },
+      ],
+      sm: [
+        '1rem',
+        {
+          lineHeight: '1.35',
+        },
+      ],
+      base: [
+        '1.25rem',
+        {
+          lineHeight: '1.35',
+        },
+      ],
+      lg: [
+        '1.5rem',
+        {
+          lineHeight: '1.25',
+        },
+      ],
+      xl: [
+        '2rem',
+        {
+          lineHeight: '1.25',
+        },
+      ],
+      '2xl': [
+        '2.5rem',
+        {
+          lineHeight: '1.15',
+        },
+      ],
+      '3xl': [
+        '3rem',
+        {
+          lineHeight: '1.15',
+        },
+      ],
       '4xl': [
         '4rem',
         {
-          lineHeight: '1',
-          letterSpacing: '0',
+          lineHeight: '1.15',
           fontWeight: '300',
         },
       ],
       '5xl': [
         '5rem',
         {
-          lineHeight: '1',
-          letterSpacing: '0',
+          lineHeight: '1.15',
           fontWeight: '300',
         },
       ],
       '6xl': [
         '6rem',
         {
-          lineHeight: '1',
-          letterSpacing: '0',
+          lineHeight: '1.15',
           fontWeight: '300',
         },
       ],
       '7xl': [
         '8rem',
         {
-          lineHeight: '1',
-          letterSpacing: '0',
+          lineHeight: '1.1',
           fontWeight: '300',
         },
       ],
       '8xl': [
         '10rem',
         {
-          lineHeight: '1',
-          letterSpacing: '0',
+          lineHeight: '1.1',
           fontWeight: '300',
         },
       ],
-      '8xl': [
+      '9xl': [
         '12rem',
         {
-          lineHeight: '1',
-          letterSpacing: '0',
+          lineHeight: '1.1',
           fontWeight: '300',
         },
       ],
