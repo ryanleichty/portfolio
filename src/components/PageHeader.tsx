58f59fdd82efc4320f3669e988848fa839b0ea72
Ryan Leichty <ryan.leichty@gmail.com>
2022-12-06 23:57:02 -0600

    Update PageHeader.tsx

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from './MaxWidthWrapper'
+import TrimText from './TrimText'
 
 export default function PageHeader({
   title,
@@ -12,15 +13,21 @@ export default function PageHeader({
   [key: string]: any
 }) {
   return (
-    <header>
-      <MaxWidthWrapper>
-        <div className="grid gap-8 pt-32 pb-16 lg:grid-cols-2 lg:gap-4">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl">{title}</h1>
-          <div className="grid grid-cols-3 gap-4">
-            <div className="col-span-full lg:text-lg xl:col-span-2">{children}</div>
-          </div>
+    <MaxWidthWrapper className="grid gap-8 pt-32 pb-16 lg:grid-cols-2 lg:gap-4">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
+        <TrimText top={-0.275} bottom={-0.15} x={-0.075}>
+          {title}
+        </TrimText>
+      </h1>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="col-span-full xl:col-span-2">
+          <p className="lg:text-lg">
+            <TrimText top={-0.275} bottom={-0.15} x={-0.075}>
+              {children}
+            </TrimText>
+          </p>
         </div>
-      </MaxWidthWrapper>
-    </header>
+      </div>
+    </MaxWidthWrapper>
   )
 }
