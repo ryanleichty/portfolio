cc3b69ed29f69ed5215497826d8129ebb8b952ab
Ryan Leichty <ryan.leichty@gmail.com>
2022-12-11 15:38:51 -0600

    Remove email and update styles

--- components/Footer.js
+++ components/Footer.js
@@ -1,35 +1,21 @@
-import { SOCIAL_PROFILES as socialProfiles } from '../constants/social';
+import { SOCIAL_PROFILES } from '../constants/social';
 
-function Footer() {
-  const socialLinks = socialProfiles.map((profile) => {
-    return (
-      <a
-        key={profile.name}
-        href={profile.slug}
-        target='_blank'
-        rel='noreferrer'
-        className='inline-block py-2 sm:py-1'
-      >
-        {profile.name}
-      </a>
-    );
-  });
-
+export default function Footer() {
   return (
-    <footer className='p-8 mt-auto font-mono text-xs leading-none uppercase tracking-[2px] whitespace-nowrap md:text-[10px]'>
-      <nav className='flex gap-8 overflow-x-scroll sm:justify-between sm:gap-4'>
-        <a
-          href='mailto:hello@ryanleichty.com'
-          target='_blank'
-          rel='noreferrer'
-          className='inline-block mr-auto py-2 sm:py-1 sm:hidden'
-        >
-          👋 hello@ryanleichty.com
-        </a>
-        {socialLinks}
+    <footer className='mt-auto whitespace-nowrap p-8 font-mono text-xs uppercase leading-none tracking-[2px] md:text-[10px]'>
+      <nav className='flex gap-8 overflow-x-scroll  sm:gap-4'>
+        {SOCIAL_PROFILES.map(({ name, slug }) => (
+          <a
+            key={name}
+            href={slug}
+            target='_blank'
+            rel='noreferrer'
+            className='inline-block py-2 sm:py-1'
+          >
+            {name}
+          </a>
+        ))}
       </nav>
     </footer>
   );
 }
-
-export default Footer;
