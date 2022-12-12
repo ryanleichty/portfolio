b2aa783e6a0d8557e6a54ce828ea5d63f669dcb2
Ryan Leichty <ryan.leichty@gmail.com>
2022-12-11 23:18:00 -0600

    Pass rest of props to component

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,3 +1,5 @@
+import { clsx } from 'clsx'
+
 import MaxWidthWrapper from '@components/MaxWidthWrapper'
 import TrimText from '@components/TrimText'
 
@@ -13,8 +15,11 @@ export default function PageHeader({
   [key: string]: any
 }) {
   return (
-    <MaxWidthWrapper className="grid gap-8 pt-32 pb-16 lg:grid-cols-2 lg:gap-4">
-      <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
+    <MaxWidthWrapper
+      className={clsx(className, 'grid gap-8 pt-32 pb-16 lg:grid-cols-2 lg:gap-4')}
+      {...props}
+    >
+      <h1 className="font-serif text-3xl md:text-4xl lg:text-5xl xl:text-8xl">
         <TrimText top={-0.275} bottom={-0.15} x={-0.075}>
           {title}
         </TrimText>
