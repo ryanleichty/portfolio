928e4dc047c661e840707d5cb1901b91d6cb686e
Ryan Leichty <ryan.leichty@gmail.com>
2022-12-12 01:17:05 -0600

    Clean up footer and update styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,34 @@
-import Link from 'next/link'
 import Button from './Button'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import TrimText from '@components/TrimText'
+
+const socialLinks = [
+  {
+    url: 'https://www.instagram.com/ryanleichty/',
+    title: 'Instagram',
+  },
+  {
+    url: 'https://twitter.com/ryanleichty',
+    title: 'Twitter',
+  },
+  {
+    url: 'https://www.linkedin.com/in/ryanleichty/',
+    title: 'LinkedIn',
+  },
+  {
+    url: 'https://github.com/ryanleichty',
+    title: 'GitHub',
+  },
+]
 
 export default function Footer() {
   return (
     <footer className="dark mt-auto bg-black text-gray-400">
       <MaxWidthWrapper className="grid gap-32 pt-16 pb-8 md:gap-96">
         <div className="grid gap-8 md:grid-cols-2 md:gap-4">
-          <div className="text-4xl md:text-6xl">
-            <TrimText top={-0.27} bottom={-0.15} x={-0.075}>
-              Say hello!
+          <div className="font-serif text-4xl italic md:text-8xl">
+            <TrimText top={0} bottom={-0.15} x={-0.075}>
+              Say hello
             </TrimText>
           </div>
           <div className="grid grid-cols-3 gap-4">
@@ -18,34 +36,24 @@ export default function Footer() {
               <TrimText top={-0.25} bottom={-0.25} x={-0.075} className="text-lg md:text-xl">
                 I’m always happy to hear from you, whether it’s about something big or small.
               </TrimText>
-              <Button variant="secondary">Email me</Button>
+              <Button variant="secondary" href="mailto:hello@ryanleichty.com">
+                Email me{'\u00A0'}↗
+              </Button>
             </div>
           </div>
         </div>
-        <div className="grid gap-12 md:grid-cols-2 md:gap-4">
-          <nav className="flex flex-col gap-2 md:order-2 md:flex-row md:gap-4">
-            <a
-              href="https://www.instagram.com/ryanleichty/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Instagram
-            </a>
-            <a href="https://twitter.com/ryanleichty" target="_blank" rel="noopener noreferrer">
-              Twitter
-            </a>
-            <a
-              href="https://www.linkedin.com/in/ryanleichty/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              LinkedIn
-            </a>
-            <a href="https://github.com/ryanleichty" target="_blank" rel="noopener noreferrer">
-              GitHub
-            </a>
+        <div className="grid gap-10 md:grid-cols-2 md:gap-4">
+          <nav className="flex flex-col flex-wrap gap-2 md:order-2 md:flex-row md:gap-6 md:gap-y-2">
+            {socialLinks.map(({ url, title }) => (
+              <a key={title} href={url} target="_blank" rel="noopener noreferrer" className="">
+                {title}
+                {'\u00A0'}↗
+              </a>
+            ))}
           </nav>
-          <div>© {new Date().getFullYear()}</div>
+          <div className="flex items-end text-xs uppercase tracking-wider">
+            © {new Date().getFullYear()} Ryan Leichty
+          </div>
         </div>
       </MaxWidthWrapper>
     </footer>
