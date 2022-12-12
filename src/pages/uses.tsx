dca91d7955f86251b6b039c855a3e569d7bf5baa
Ryan Leichty <ryan.leichty@gmail.com>
2022-12-12 01:16:17 -0600

    Update uses page

--- src/pages/uses.tsx
+++ src/pages/uses.tsx
@@ -1,5 +1,8 @@
 import Head from '@components/Head'
+import MaxWidthWrapper from '@components/MaxWidthWrapper'
 import PageBanner from '@components/PageBanner'
+import Sandbox from '@components/Sandbox'
+
 const pageMeta = {
   title: 'Uses',
   description:
@@ -11,9 +14,18 @@ export default function UsesPage() {
     <>
       <Head title={pageMeta.title} description={pageMeta.description} />
       <PageBanner title="Uses">
-        The details of my developer setup, software and tech. Check out uses.tech for a list of
-        other peoples uses pages.
+        The details of my developer setup, software and tech. Check out{' '}
+        <a href="https://uses.tech/" target="_blank" rel="noopener noreferrer">
+          uses.tech
+        </a>{' '}
+        for a list of other peoples uses pages.
       </PageBanner>
+
+      <div className="aspect-[3/2] h-full w-full bg-black"></div>
+
+      <MaxWidthWrapper className="py-8">
+        <Sandbox />
+      </MaxWidthWrapper>
     </>
   )
 }
