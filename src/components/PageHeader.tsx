830eb10d8122b1a09d871747e98411ac34e57d06
Ryan Leichty <ryan.leichty@gmail.com>
2022-12-12 01:12:34 -0600

    Update styles

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -19,8 +19,8 @@ export default function PageHeader({
       className={clsx(className, 'grid gap-8 pt-32 pb-16 lg:grid-cols-2 lg:gap-4')}
       {...props}
     >
-      <h1 className="font-serif text-3xl md:text-4xl lg:text-5xl xl:text-8xl">
-        <TrimText top={-0.275} bottom={-0.15} x={-0.075}>
+      <h1 className="font-serif text-3xl italic md:text-4xl lg:text-5xl xl:text-8xl">
+        <TrimText top={0} bottom={-0.15} x={-0.075}>
           {title}
         </TrimText>
       </h1>
