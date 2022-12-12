2ef4f6956ad017bfc57f82412519b449225cda30
Ryan Leichty <ryan.leichty@gmail.com>
2022-12-12 01:16:06 -0600

    Update work page

--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -1,3 +1,21 @@
+import Head from '@components/Head'
+import PageBanner from '@components/PageBanner'
+
+const pageMeta = {
+  title: 'Work',
+  description: 'The work page description...',
+}
+
 export default function WorkPage() {
-  return <h1>Work</h1>
+  return (
+    <>
+      <Head title={pageMeta.title} description={pageMeta.description} />
+      <PageBanner title="Work">
+        The details of my developer setup, software and tech. Check out uses.tech for a list of
+        other peoples uses pages.
+      </PageBanner>
+
+      <div className="aspect-[3/2] h-full w-full bg-black"></div>
+    </>
+  )
 }
