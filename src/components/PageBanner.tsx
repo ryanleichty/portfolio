671d7a5db63879c2ff4c153a3273a6a3f2696303
Ryan Leichty <ryan.leichty@gmail.com>
2022-12-12 01:12:13 -0600

    Include className in props
    Added className to props to allow additional classes to be added to the outer element. Updated other styles too

--- src/components/PageBanner.tsx
+++ src/components/PageBanner.tsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion'
+import { clsx } from 'clsx'
 
 import MaxWidthWrapper from '@components/MaxWidthWrapper'
 import TrimText from '@components/TrimText'
@@ -36,22 +37,22 @@ export default function PageBanner({
   [key: string]: any
 }) {
   return (
-    <MaxWidthWrapper className="border-b border-black" {...props}>
+    <MaxWidthWrapper className={clsx('border-b border-black', className)} {...props}>
       <motion.div
         initial="hidden"
         animate="visible"
         variants={motionParent}
-        className="flex min-h-screen-no-header flex-col items-center justify-between gap-16 py-16 text-center"
+        className="flex min-h-screen-no-header flex-col items-center justify-between gap-16 py-12 text-center md:py-16"
       >
         <motion.h1
           variants={motionChild}
-          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl"
+          className="font-serif text-4xl italic sm:text-5xl md:text-6xl lg:text-7xl xl:text-9xl"
         >
-          <TrimText top={-0.25} bottom={-0.15} x={-0.075}>
+          <TrimText top={0} bottom={-0.15} x={-0.075}>
             {title}
           </TrimText>
         </motion.h1>
-        <motion.p variants={motionChild} className="max-w-lg text-lg">
+        <motion.p variants={motionChild} className="max-w-lg text-sm sm:text-base lg:text-lg">
           <TrimText top={-0.25} bottom={-0.25} x={-0.075}>
             {children}
           </TrimText>
