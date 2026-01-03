0dc70f5361622c6275fcf1f48244ef64c06c5073
Ryan Leichty <ryan.leichty@gmail.com>
2026-01-03 01:28:32 -0600

    Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
     default: 'Ryan Leichty',
     template: '%s | Ryan Leichty',
   },
-  description: 'My portfolio, blog, and personal website.',
+  description: 'I’m a developer and designer.',
 }
 
 export default function RootLayout({
@@ -23,9 +23,12 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en' className={`${inter.className}`}>
+    <html
+      lang='en'
+      className={`${inter.className} bg-white text-gray-900 antialiased dark:bg-zinc-950 dark:text-zinc-200`}
+    >
       <body className='tracking-tight antialiased'>
-        <div className='flex min-h-screen flex-col justify-between bg-white p-8 pt-0 text-gray-900 md:pt-8 dark:bg-zinc-950 dark:text-zinc-200'>
+        <div className='flex min-h-screen flex-col justify-between p-8 pt-0 md:pt-8'>
           <main className='mx-auto w-full max-w-[60ch] space-y-6'>{children}</main>
           <Footer />
           <Analytics />
