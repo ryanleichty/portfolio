edbce029dc68e8dc3dd695fd2d7a8332d4749ad3
Ryan Leichty <ryan.leichty@gmail.com>
2026-01-03 01:40:21 -0600

    More updates

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -10,27 +10,27 @@ type AnchorProps = ComponentPropsWithoutRef<'a'>
 type BlockquoteProps = ComponentPropsWithoutRef<'blockquote'>
 
 const components = {
-  h1: (props: HeadingProps) => <h1 className='mb-0 pt-12 font-medium' {...props} />,
+  h1: (props: HeadingProps) => <h1 className="mb-0 pt-12 font-medium" {...props} />,
   h2: (props: HeadingProps) => (
-    <h2 className='mt-8 mb-3 font-medium text-gray-800 dark:text-zinc-200' {...props} />
+    <h2 className="mt-8 mb-3 font-medium text-gray-800 dark:text-zinc-200" {...props} />
   ),
   h3: (props: HeadingProps) => (
-    <h3 className='mt-8 mb-3 font-medium text-gray-800 dark:text-zinc-200' {...props} />
+    <h3 className="mt-8 mb-3 font-medium text-gray-800 dark:text-zinc-200" {...props} />
   ),
-  h4: (props: HeadingProps) => <h4 className='font-medium' {...props} />,
+  h4: (props: HeadingProps) => <h4 className="font-medium" {...props} />,
   p: (props: ParagraphProps) => (
-    <p className='leading-snug text-gray-800 dark:text-zinc-300' {...props} />
+    <p className="leading-snug text-gray-800 dark:text-zinc-300" {...props} />
   ),
   ol: (props: ListProps) => (
-    <ol className='list-decimal space-y-2 pl-5 text-gray-800 dark:text-zinc-300' {...props} />
+    <ol className="list-decimal space-y-2 pl-5 text-gray-800 dark:text-zinc-300" {...props} />
   ),
   ul: (props: ListProps) => (
-    <ul className='list-disc space-y-1 pl-5 text-gray-800 dark:text-zinc-300' {...props} />
+    <ul className="list-disc space-y-1 pl-5 text-gray-800 dark:text-zinc-300" {...props} />
   ),
-  li: (props: ListItemProps) => <li className='pl-1' {...props} />,
-  em: (props: ComponentPropsWithoutRef<'em'>) => <em className='font-medium' {...props} />,
+  li: (props: ListItemProps) => <li className="pl-1" {...props} />,
+  em: (props: ComponentPropsWithoutRef<'em'>) => <em className="font-medium" {...props} />,
   strong: (props: ComponentPropsWithoutRef<'strong'>) => (
-    <strong className='font-medium' {...props} />
+    <strong className="font-medium" {...props} />
   ),
   a: ({ href, children, ...props }: AnchorProps) => {
     const className =
@@ -50,7 +50,7 @@ const components = {
       )
     }
     return (
-      <a href={href} target='_blank' rel='noopener noreferrer' className={className} {...props}>
+      <a href={href} target="_blank" rel="noopener noreferrer" className={className} {...props}>
         {children}
       </a>
     )
@@ -81,7 +81,7 @@ const components = {
   ),
   blockquote: (props: BlockquoteProps) => (
     <blockquote
-      className='ml-[0.075em] border-l-3 border-gray-300 pl-4 text-gray-700 dark:border-zinc-600 dark:text-zinc-300'
+      className="ml-[0.075em] border-l-3 border-gray-300 pl-4 text-gray-700 dark:border-zinc-600 dark:text-zinc-300"
       {...props}
     />
   ),
